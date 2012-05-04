6918fbff488f722351ab1f2c5fac67ae4eec04a6
Francois VANTOMME <francois@pleinnet.fr>
2012-05-04 11:31:46 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -11,25 +11,27 @@ from setuptools import setup, find_packages
 # setup itself.
 setup(
     name = "Django image sitemaps",
-    version = '1.0 alpha',
+    version = '1.0',
     install_requires = ['django>=1.3'],
     packages = find_packages(),
     author = "Francois Vantomme",
     author_email = "akarzim@gmail.com",
     description = "Google Image Sitemaps builder for Django.",
-    license = "LICENSE",
+    license = "BSD License",
     keywords = "google, django, image, sitemap",
     url = "https://github.org/akarzim/image-sitemaps/",
     include_package_data = True,
     classifiers=[
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 5 - Production/Stable",
         "Environment :: Web Environment",
         "Framework :: Django",
         "Intended Audience :: Developers",
-        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
-        "Topic :: Software Development"
+        "Topic :: Software Development",
+        "Topic :: Internet",
+        "Natural Language :: English"
     ],
 )
 
