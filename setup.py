8d6ee943abd790d02d038c525df19cc6aae29916
François Vantomme <akarzim@gmail.com>
2020-10-25 16:14:29 +0100

    Chore: bump version to 1.1.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup, find_packages
 # setup itself.
 setup(
     name = "django-image-sitemaps",
-    version = '1.05',
+    version = '1.1.0',
     install_requires = ['django>=1.3'],
     packages = find_packages(),
     data_files=[('imagesitemaps/templates', ['imagesitemaps/templates/image_sitemap.xml'])],
