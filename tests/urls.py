43460c3cc77e796f463648ab18505198042f2846
Petr Dlouhý <petr.dlouhy@email.cz>
2022-08-23 13:31:28 +0200

    add configuration for GitHub testing

---
+++ tests/urls.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+
+from django.contrib import admin
+from django.urls import path
+
+
+urlpatterns = [
+    path("admin", admin.site.urls),
+]
